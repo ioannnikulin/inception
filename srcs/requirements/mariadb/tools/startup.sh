6bf62d2031bb207315d9af174b0a5ba972f35f9e
Ivan Nikulin <inikulin@c4a10c3.42berlin.de>
2025-08-30 19:22:47 +0200

    mariadb

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -1,9 +1,28 @@
-service mysql start
-mysql -e "\
-	CREATE DATABASE IF NOT EXISTS \`$db_name\`; \
-	CREATE USER IF NOT EXISTS '$db_username'@'%' IDENTIFIED BY '$db_password'; \
-	GRANT ALL PRIVILEGES ON \`$db_name\`.* TO '$db_username'@'%'; \
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '$db_root_password'; \
-	FLUSH PRIVILEGES; \
-	"
-mysqld
+#!/bin/bash
+
+mkdir -p /var/run/mysqld # if it doesn't exist or belongs to root, daemon fails
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+
+mysqld --initialize-insecure --user=mysql
+
+mysqld --skip-networking --socket=/var/run/mysqld/mysqld.sock &
+pid="$!" # last background process ID
+
+until mysqladmin ping --silent; do
+  echo "Waiting for MariaDB..."
+  sleep 1
+done
+
+mysql -u root <<-EOSQL
+  CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
+  CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+  GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%';
+  ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
+  FLUSH PRIVILEGES;
+EOSQL
+
+# stop temporary server
+kill "$pid"
+wait "$pid"
+
+exec mysqld
